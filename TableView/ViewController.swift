46b66e12baebe76333c749e5fb31b93d6c40a3f2
Gaspar Teixeira <gaspar.teixeira@gmail.com>
2016-09-12 22:05:50 -0300

    this commit has a simple project with a tableview + constraint to most common devices's screen

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,11 +8,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var dataSource : NSMutableArray = NSMutableArray()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0...20
+        {
+            let tempName : String  = "Gaspar \(i)"
+            let tempLastName : String = "Teixeira \(i)"
+            let tempUser : NSArray = NSArray(objects: tempName, tempLastName)
+            self.dataSource.addObject(tempUser)
+            
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.dataSource.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        let myObject : NSArray = self.dataSource.objectAtIndex(indexPath.row) as! NSArray
+        let myMessage : String = (myObject.objectAtIndex(0) as! String) + " " + (myObject.objectAtIndex(1) as! String)
+        print(myMessage)
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let myId : String = "example"
+        
+        var cell : UITableViewCell! = tableView.dequeueReusableCellWithIdentifier(myId)
+        
+        if cell == nil
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: myId)
+        }
+        
+        let myObject : NSArray = self.dataSource.objectAtIndex(indexPath.row) as! NSArray
+        
+        cell.textLabel?.text = myObject.objectAtIndex(0) as? String
+        cell.detailTextLabel?.text = myObject.objectAtIndex(1) as? String
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
